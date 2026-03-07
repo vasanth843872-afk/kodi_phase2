da820055068828090ecb4b2b9fbe756c1923941e
vasanth843872-afk <vasanth843872@gmail.com>
2026-03-07 09:35:28 +0000

    added allowed_host

--- kodi_core/settings.py
+++ kodi_core/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY', default='your-secret-key-here')
 DEBUG = True
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['192.168.1.10','127.0.0.1']
+ALLOWED_HOSTS = ['192.168.1.10','127.0.0.1','kodi-phase2.onrender.com']
 CORS_ALLOW_ALL_ORIGINS = True 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
